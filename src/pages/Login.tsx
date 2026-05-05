5c8656bd4155d020a474455a2ac5195734c00e03
keiranbun <117739313+keiranbun@users.noreply.github.com>
2026-05-05 18:41:26 +1000

    feat(login): add login button/styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,16 @@
 import "../styles/login.css";
 
 const Login = () => {
+  const handleLogin = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("WIP Login");
+
+    // TODO: Add login system
+  };
+
   return (
     <div className="login-page flex-center align-center">
-      <form autoComplete="off">
+      <form autoComplete="off" onSubmit={(e) => handleLogin(e)}>
         <div className="login-container">
           <div className="input-container">
             <label htmlFor="username">username</label>
@@ -24,6 +31,11 @@ const Login = () => {
               className="login-input"
               required
             />
+          </div>
+          <div className="flex-right">
+            <button type="submit" className="login-button">
+              login
+            </button>
           </div>
         </div>
       </form>
