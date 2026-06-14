d1d02c381511a0ef1fa430fc0d942160edb6c5d7
keiranbun <117739313+keiranbun@users.noreply.github.com>
2026-06-14 17:54:43 +1000

    style(Error): update error page layout and styling
    - `src/pages/Error.tsx` - modify layout to use Tailwind CSS classes and update text styles

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,16 +1,8 @@
-import "../styles/error.css";
-
 const Error = () => {
   return (
-    <div className="flex-center flex-column align-center">
-      <h1 className="header">404</h1>
-      <h2 className="sub-header">page not found</h2>
-      <p>
-        return to the{" "}
-        <a href="/" className="link">
-          homepage
-        </a>
-      </p>
+    <div className="flex flex-col items-center text-lg">
+      <h1 className="text-primary">404</h1>
+      <h2>[page not found]</h2>
     </div>
   );
 };
